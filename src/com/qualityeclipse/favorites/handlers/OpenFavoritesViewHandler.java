cab3948a867cd643606bc3df8a5fadfa02037034
zhengzhe <zhe_zheng@126.com>
2012-11-11 11:27:09 +0800

    chap06-3-1 add handler
    finished add handler

--- src/com/qualityeclipse/favorites/handlers/OpenFavoritesViewHandler.java
+++ src/com/qualityeclipse/favorites/handlers/OpenFavoritesViewHandler.java
@@ -1,10 +1,14 @@
 package com.qualityeclipse.favorites.handlers;
 
-import java.util.Map;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.qualityeclipse.favorites.views.FavoriteView;
 
 
 public class OpenFavoritesViewHandler extends AbstractHandler {
@@ -14,6 +18,17 @@ public class OpenFavoritesViewHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
 		//TODO:05添加打开视图的相关代码
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		if (window==null) {
+			return null;
+		}
+		
+		IWorkbenchPage  activePage = window.getActivePage();
+		try {
+			activePage.showView(FavoriteView.ID);
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
