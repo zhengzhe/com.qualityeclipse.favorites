6bd161bba55a67a8592eb40df75c1d1f83141b75
zhengzhe <zhe_zheng@126.com>
2012-11-11 11:16:53 +0800

    chap06-3 add handler
    add OpenFavoritesViewHandler

---
+++ src/com/qualityeclipse/favorites/handlers/OpenFavoritesViewHandler.java
@@ -0,0 +1,20 @@
+package com.qualityeclipse.favorites.handlers;
+
+import java.util.Map;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+public class OpenFavoritesViewHandler extends AbstractHandler {
+
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		//TODO:05添加打开视图的相关代码
+		return null;
+	}
+
+}
